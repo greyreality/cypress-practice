25f31cfa526700b8ad1d542db4d6f1d166a19b97
Margarita Leontyeva <apelsinka_89@mail.ru>
2022-02-04 16:09:57 +0700

    move examples into ignored folder

--- cypress/integration/examples/01_testtask_manatal.spec.js
+++ cypress/integration/examples/01_testtask_manatal.spec.js
@@ -1,10 +1,9 @@
 /// <reference types="cypress" />
 import {loginPage} from "../../integration/examples/pages/loginpage"
 
-describe("Manatal testing", () => {
-
+describe("Run tests for Manatal login page", () => {
+  // Common steps for all testcases
   beforeEach("Open website",() => {
-    // Common steps for all testcases
     cy.visit("/signup");
     cy.fixture("login_testdata").as("testdata");
   })
@@ -18,22 +17,16 @@ describe("Manatal testing", () => {
 
     cy.get("@testdata").then((testdata) => {
        loginPage.fillinRequiredFields(testdata.name,testdata.organization_name,testdata.password,testdata.phone)
-        // // Using custom command function
-        // cy.fillin_username(testdata.name);
-        // // Using testdata from fixture
-        // cy.get("#organization_name").type(testdata.organization_name);
-        // cy.get("#password").type(testdata.password);
-        // cy.get("[data-vv-name=phone]").type(testdata.phone);
     })
     
     //Verify radiobuttons
     cy.findByLabelText('Agency', { timeout: 7000 }).should('exist')
     cy.findByLabelText('Company', { timeout: 7000 }).should('exist')
 
-    cy.get("#company_email_address").type("rita.leontyeva+26@candidate.manatal.com")
-    cy.get("#confirm_company_email_address").type("rita.leontyeva+26@candidate.manatal.com")
+    cy.get("#company_email_address").type("rita.leontyeva+28@candidate.manatal.com")
+    cy.get("#confirm_company_email_address").type("rita.leontyeva+28@candidate.manatal.com")
     cy.get("[data-vv-name=agree]").check()
-    // cy.contains("Sign up").click()
+
     loginPage.clickSingUpButton();
 
     // Wait until confirm email page will be loaded
@@ -45,7 +38,7 @@ describe("Manatal testing", () => {
       const confirmText = $confirm.text();
       expect(confirmText).is.eql("A confirmation email was sent to");
     });
-    cy.contains("rita.leontyeva+26@candidate.manatal.com")
+    cy.contains("rita.leontyeva+28@candidate.manatal.com")
 
   })
 
